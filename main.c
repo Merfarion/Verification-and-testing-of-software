636a3e7e655ca1a68d0569220b19e88202556b74
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-12-19 01:32:58 +0300

    добавлен код для еста

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-/*
+
+
+ /*
 	FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
 	All rights reserved
 
@@ -56,85 +58,164 @@
 /* FreeRTOS.org includes. */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
 
 /* Demo includes. */
-//#include "supporting_functions.h"
+#include "supporting_functions.h"
 
-/* Used as a loop counter to create a very crude delay. */
-#define mainDELAY_LOOP_COUNT		( 0xffffff )
-
-/* The task functions. */
-void vTask1(void* pvParameters);
-void vTask2(void* pvParameters);
+/* The tasks to be created.  Two instances are created of the sender task while
+only a single instance is created of the receiver task. */
+static void vSenderTask(void* pvParameters);
+static void vReceiverTask(void* pvParameters);
 
 /*-----------------------------------------------------------*/
 
+/* Declare a variable of type QueueHandle_t.  This is used to store the queue
+that is accessed by all three tasks. */
+
+QueueHandle_t xQueue;
+typedef enum
+{
+	eSender1,
+	eSender2
+} DataSource_t;
+
+/* Define the structure type that will be passed on the queue. */
+typedef struct
+{
+	uint8_t ucValue;
+	DataSource_t eDataSource;
+} Data_t;
+
+/* Declare two variables of type Data_t that will be passed on the queue. */
+static const Data_t xStructsToSend[2] =
+{
+	{ 100, eSender1 }, /* Used by Sender1. */
+	{ 200, eSender2 }  /* Used by Sender2. */
+};
+
 int main(void)
 {
-	/* Create one of the two tasks. */
-	xTaskCreate(vTask1,		/* Pointer to the function that implements the task. */
-		"Task 1",	/* Text name for the task.  This is to facilitate debugging only. */
-		1000,		/* Stack depth - most small microcontrollers will use much less stack than this. */
-		NULL,		/* We are not using the task parameter. */
-		2,			/* This task will run at priority 1. */
-		NULL);		/* We are not using the task handle. */
+	/* The queue is created to hold a maximum of 3 structures of type Data_t. */
+	xQueue = xQueueCreate(3, sizeof(Data_t));
 
-/* Create the other task in exactly the same way. */
-	xTaskCreate(vTask2, "Task 2", 1000, NULL, 2, NULL);
+	if (xQueue != NULL)
+	{
+		/* Create two instances of the task that will write to the queue.  The
+		parameter is used to pass the structure that the task should write to the
+		queue, so one task will continuously send xStructsToSend[ 0 ] to the queue
+		while the other task will continuously send xStructsToSend[ 1 ].  Both
+		tasks are created at priority 2, which is above the priority of the receiver. */
+		xTaskCreate(vSenderTask, "Sender1", 1000, (void*)&(xStructsToSend[0]), 2, NULL);
+		xTaskCreate(vSenderTask, "Sender2", 1000, (void*)&(xStructsToSend[1]), 2, NULL);
 
-	/* Start the scheduler to start the tasks executing. */
-	vTaskStartScheduler();
+		/* Create the task that will read from the queue.  The task is created with
+		priority 1, so below the priority of the sender tasks. */
+		xTaskCreate(vReceiverTask, "Receiver", 1000, NULL, 1, NULL);
+
+		/* Start the scheduler so the created tasks start executing. */
+		vTaskStartScheduler();
+	}
+	else
+	{
+		/* The queue could not be created. */
+	}
 
 	/* The following line should never be reached because vTaskStartScheduler()
 	will only return if there was not enough FreeRTOS heap memory available to
 	create the Idle and (if configured) Timer tasks.  Heap management, and
 	techniques for trapping heap exhaustion, are described in the book text. */
-	for (;; ); //  бесконечный цикл
-	return 0; // Завершение выполнения главной функции
+	for (;; );
+	return 0;
 }
 /*-----------------------------------------------------------*/
 
-void vTask1(void* pvParameters)
+static void vSenderTask(void* pvParameters)
 {
-	const char* pcTaskName = "Task 1 is running\r\n";
-	volatile uint32_t ul;
+	BaseType_t xStatus;
+	const TickType_t xTicksToWait = pdMS_TO_TICKS(100UL);
 
-	/* As per most tasks, this task is implemented in an infinite loop. */
-	for (;; )//  бесконечный цикл
+	/* As per most tasks, this task is implemented within an infinite loop. */
+	for (;; )
 	{
-		/* Print out the name of this task. */
-		vPrintString(pcTaskName);
+		/* The first parameter is the queue to which data is being sent.  The
+		queue was created before the scheduler was started, so before this task
+		started to execute.
 
-		/* Delay for a period. */
-		for (ul = 0; ul < mainDELAY_LOOP_COUNT; ul++) //Цикл на создание задержки на некоторый период времени
+		The second parameter is the address of the structure being sent.  The
+		address is passed in as the task parameter.
+
+		The third parameter is the Block time - the time the task should be kept
+		in the Blocked state to wait for space to become available on the queue
+		should the queue already be full.  A block time is specified as the queue
+		will become full.  Items will only be removed from the queue when both
+		sending tasks are in the Blocked state.. */
+		xStatus = xQueueSendToBack(xQueue, pvParameters, xTicksToWait);
+
+		if (xStatus != pdPASS)
 		{
-			/* This loop is just a very crude delay implementation.  There is
-			nothing to do in here.  Later exercises will replace this crude
-			loop with a proper delay/sleep function. */
+			/* We could not write to the queue because it was full - this must
+			be an error as the receiving task should make space in the queue
+			as soon as both sending tasks are in the Blocked state. */
+			vPrintString("Could not send to the queue.\r\n");
 		}
 	}
 }
 /*-----------------------------------------------------------*/
 
-void vTask2(void* pvParameters)
+static void vReceiverTask(void* pvParameters)
 {
-	const char* pcTaskName = "Task 2 is running\r\n";
-	volatile uint32_t ul;
+	/* Declare the structure that will hold the values received from the queue. */
+	Data_t xReceivedStructure;
+	BaseType_t xStatus;
 
-	/* As per most tasks, this task is implemented in an infinite loop. */
-	for (;; )//  бесконечный цикл
+	/* This task is also defined within an infinite loop. */
+	for (;; )
 	{
-		/* Print out the name of this task. */
-		vPrintString(pcTaskName);
-
-		/* Delay for a period. */
-		for (ul = 0; ul < mainDELAY_LOOP_COUNT; ul++)//Задержка на некоторый период времени
+		/* As this task only runs when the sending tasks are in the Blocked state,
+		and the sending tasks only block when the queue is full, this task should
+		always find the queue to be full.  3 is the queue length. */
+		if (uxQueueMessagesWaiting(xQueue) != 3)
 		{
-			/* This loop is just a very crude delay implementation.  There is
-			nothing to do in here.  Later exercises will replace this crude
-			loop with a proper delay/sleep function. */
+			vPrintString("Queue should have been full!\r\n");
+		}
+
+		/* The first parameter is the queue from which data is to be received.  The
+		queue is created before the scheduler is started, and therefore before this
+		task runs for the first time.
+
+		The second parameter is the buffer into which the received data will be
+		placed.  In this case the buffer is simply the address of a variable that
+		has the required size to hold the received structure.
+
+		The last parameter is the block time - the maximum amount of time that the
+		task should remain in the Blocked state to wait for data to be available
+		should the queue already be empty.  A block time is not necessary as this
+		task will only run when the queue is full so data will always be available. */
+		xStatus = xQueueReceive(xQueue, &xReceivedStructure, 0);
+
+		if (xStatus == pdPASS)
+		{
+			/* Data was successfully received from the queue, print out the received
+			value and the source of the value. */
+			if (xReceivedStructure.eDataSource == eSender1)
+			{
+				vPrintStringAndNumber("From Sender 1 = ", xReceivedStructure.ucValue);
+			}
+			else
+			{
+				vPrintStringAndNumber("From Sender 2 = ", xReceivedStructure.ucValue);
+			}
+		}
+		else
+		{
+			/* We did not receive anything from the queue.  This must be an error
+			as this task should only run when the queue is full. */
+			vPrintString("Could not receive from the queue.\r\n");
 		}
 	}
 }
+
+
 
 
